6f8f783e2c2ee97d9b7701e3e2ca70339ab3b4e6
MdFaizanuddin1 <md18faizan@gmail.com>
2023-12-09 12:23:20 +0530

    Add delete user functionality

--- client/src/fireBase.js
+++ client/src/fireBase.js
@@ -15,3 +15,23 @@ const firebaseConfig = {
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
+
+// Import the functions you need from the SDKs you need
+//import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+
+// mern estate1
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+//   authDomain: "mern-estate-new.firebaseapp.com",
+//   projectId: "mern-estate-new",
+//   storageBucket: "mern-estate-new.appspot.com",
+//   messagingSenderId: "916814617541",
+//   appId: "1:916814617541:web:6116cc2caeb58ae4dc09ac",
+// };
+
+// // Initialize Firebase
+// export const app = initializeApp(firebaseConfig);
