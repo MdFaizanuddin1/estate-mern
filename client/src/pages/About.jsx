b826a867d624ff7ee4b433618ff25286d8c05f7c
MdFaizanuddin1 <md18faizan@gmail.com>
2023-12-14 15:40:48 +0530

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
 
 function About() {
   return (
-    <div>About</div>
-  )
+    <div className=" py-20 px-4 max-w-6xl mx-auto">
+      <h1 className=" text-3xl font-bold mb-4 text-slate-800">
+        About Faizan Estate
+      </h1>
+      <p className=" mb-4 text-slate-700">
+        Faizan Estate, your premier online real estate dealership, stands at the
+        forefront of the digital evolution in property transactions. With a
+        commitment to innovation and customer satisfaction, Faizan Estate
+        harnesses the power of technology to provide a seamless and efficient
+        experience for clients navigating the dynamic real estate market.
+      </p>
+      <p className=" mb-4 text-slate-700">
+        Faizan Estate's online platform serves as a comprehensive hub for
+        property seekers and sellers alike. Through an intuitive website,
+        clients can explore an extensive portfolio of listings, featuring a
+        diverse range of residential, commercial, and investment properties. The
+        user-friendly interface ensures easy navigation, allowing visitors to
+        refine their search based on specific criteria, such as location,
+        budget, and property type.
+      </p>
+      <p className=" mb-4 text-slate-700">
+        The integration of cutting-edge tools and technologies sets Faizan
+        Estate apart in the digital realm. From virtual property tours and
+        high-quality images to interactive maps and real-time market analyses,
+        the online platform offers a wealth of information to empower clients in
+        their decision-making process.
+      </p>
+    </div>
+  );
 }
 
-export default About
+export default About;
